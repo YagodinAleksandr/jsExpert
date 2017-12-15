417dd2049c3d055a025726ba0c8d8207706d7570
Alexandr Iagodin <Alexandr@Marinas-MacBook-Pro.local>
2017-12-15 16:57:42 +0200

    adding 6.3

---
+++ 6/6.3/js/main.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var value;
+console.log(value);
+value = true;
+console.log(value);
+value = 101;
+console.log(value);
+value = "number";
+console.log(value);
+value = null;
+console.log(value);
+console.log(typeof(value));
+
