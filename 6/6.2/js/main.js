a8516955992759444d04db87fdb86f0194846d85
Alexandr Iagodin <Alexandr@Marinas-MacBook-Pro.local>
2017-12-15 16:40:19 +0200

    adding 6.2

---
+++ 6/6.2/js/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var booleanValue, stringValue, numberValue;
+booleanValue = stringValue = numberValue = 1;
+console.log(booleanValue, stringValue, numberValue);
+booleanValue = true;
+stringValue = "1";
+console.log(booleanValue, stringValue, numberValue);
