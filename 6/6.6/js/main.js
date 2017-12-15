426e32cbdad19173267ad2872ac44188658e4ba7
Alexandr Iagodin <Alexandr@Marinas-MacBook-Pro.local>
2017-12-15 18:00:23 +0200

    adding 6.6

---
+++ 6/6.6/js/main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var value = 5;
+value = String(value);
+console.log(typeof(value));
+value = Boolean(value);
+console.log(typeof(value));
+value = +value;
+console.log(typeof(value));
+
+
